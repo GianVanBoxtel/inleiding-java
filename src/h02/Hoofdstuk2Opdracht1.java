323604f534286b360ff6d26b11cf3de0a6de8779
boxso <gian@boxsol.nl>
2020-10-23 13:17:54 +0200

    3

---
+++ src/h02/Hoofdstuk2Opdracht1.java
@@ -0,0 +1,19 @@
+package h02;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Hoofdstuk2Opdracht1 extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Gian", 50, 60);
+    }
+
+}
+
+
